da27ba59d56c3f1fe56c78ca767972d835101010
Janxxy <lesley.pond@gmail.com>
2019-05-06 11:07:11 -0400

    components for user data

---
+++ client/src/components/LogUserData/LogUserData.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class LogUserData extends React.Component{
+
+    render(){
+        return(
+            <div>
+                 <h5>Enter how you are feeling today /date here/</h5>
+            </div>
+           
+        )
+    }
+}
+
+export default LogUserData;
