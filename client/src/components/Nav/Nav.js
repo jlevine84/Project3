c4ba799b6f2eca791bccf3c1357cb5984c75187f
Jeff Levine <jlevine_84@yahoo.com>
2019-05-20 18:15:33 -0400

    More linting

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,17 +5,17 @@ import './Nav.css';
 class Nav extends React.Component {
  
   render () {
-  return (
-    <nav class="navbar">
-     <Link to="/"> <img src='/images/icon.png' className="littleicon"></img></Link>
-      <h5>Welcome, {this.props.user}!</h5>
-      <Link to="/about" className = "navbarlink">About Dadirri</Link>
-      <Link to="/dashboard" className="navbarlink">Dashboard</Link>
-      <button type="button" className="btn btn-info" onClick = {this.props.logout}>Logout</button>
-      
-    </nav>
-  )
-    }
+    return (
+      <nav class="navbar">
+      <Link to="/"> <img src='/images/icon.png' className="littleicon"></img></Link>
+        <h5>Welcome, {this.props.user}!</h5>
+        <Link to="/about" className = "navbarlink">About Dadirri</Link>
+        <Link to="/dashboard" className="navbarlink">Dashboard</Link>
+        <button type="button" className="btn btn-info" onClick = {this.props.logout}>Logout</button>
+        
+      </nav>
+    )
+  }
 };
 
 export default Nav;
--- client/src/components/ViewUserData/ViewUserData.js
+++ client/src/components/ViewUserData/ViewUserData.js
@@ -31,11 +31,13 @@ class ViewUserData extends React.Component {
            edit:true
         })   
     }
+    
     switchtoSeeData = () =>{
         this.setState({
             edit: false
         })
     }
+
     componentDidMount(){
         API.getByDate(this.props.selectedDate)
         .then(async response=>{
@@ -48,21 +50,22 @@ class ViewUserData extends React.Component {
     componentWillReceiveProps() {
         API.getByDate(this.props.selectedDate)
         .then(async response=>{
-            if (response.data.todaysentry[0]){
+            if (response.data.todaysentry[0]) {
                 await this.setState({ logged: true })
-            } else await this.setState({ 
-                    Mood: "5",
-                    Anxiety: "5",
-                    Energy: "5",
-                    MedicineTaken: false,
-                    Exercise: false,
-                    SleepHours: "0",
-                    DailyLog: "",
-                    ExerciseAmount: "",
-                    Showered: false,
-                    logged: false, 
-                    viewDate: moment(this.props.selectedDate, 'YYYYMMDD').format('MMMM Do YYYY'), 
-                })
+            } 
+            else await this.setState({ 
+                Mood: "5",
+                Anxiety: "5",
+                Energy: "5",
+                MedicineTaken: false,
+                Exercise: false,
+                SleepHours: "0",
+                DailyLog: "",
+                ExerciseAmount: "",
+                Showered: false,
+                logged: false, 
+                viewDate: moment(this.props.selectedDate, 'YYYYMMDD').format('MMMM Do YYYY'), 
+            })
         }).catch(err => console.log(err))
     }
 
@@ -101,126 +104,121 @@ class ViewUserData extends React.Component {
     render(){
         const entryDate = moment(this.props.selectedDate, 'YYYYMMDD').format('MMMM Do YYYY')
         return(
-         <div>
-             {(this.state.logged && !this.state.edit) ?
-             <div className ="jumbotron">
-             <div>
-                 <h5>Data for {entryDate}</h5>
-                 <p><strong>Mood: </strong>{this.props.mood}</p>
-                 <p><strong>Anxiety: </strong>{this.props.anxiety}</p>
-                 <p><strong>Energy: </strong>{this.props.energy}</p>
-                 <p><strong>Hours Slept: </strong>{this.props.sleepHours}</p>
-                 <p><strong>Medicine Taken: </strong>{this.props.medicineTaken}</p>
-                 <p><strong>Showered: </strong>{this.props.showered}</p>
-                 {this.props.exercise === "true" ?  
-                    <div>
-                    <p><strong>Exercised: </strong>{this.props.exercise}</p>
-                    <p><strong>Exercise Details: </strong>
-                    <br></br>
-                    {this.props.exerciseAmount}</p>
-                    </div>: ""
-                }
-                 <p><strong>Daily Log: </strong>
-                 <br></br>
-                 {this.props.dailyLog}</p>
-
-                 <button type="button" className="btn btn-success float2" onClick={this.switchToEdit}>Edit this entry</button>
-             </div>
-         
-             </div>
-             : (
-                <div className="jumbotron">
-                <div className="row">
-                    <div className="col-sm-12 top">
-                        <h5>New Entry for: {entryDate}</h5>
+            <div>
+                {(this.state.logged && !this.state.edit) ?
+                    <div className ="jumbotron">
+                        <div>
+                            <h5>Data for {entryDate}</h5>
+                            <p><strong>Mood: </strong>{this.props.mood}</p>
+                            <p><strong>Anxiety: </strong>{this.props.anxiety}</p>
+                            <p><strong>Energy: </strong>{this.props.energy}</p>
+                            <p><strong>Hours Slept: </strong>{this.props.sleepHours}</p>
+                            <p><strong>Medicine Taken: </strong>{this.props.medicineTaken}</p>
+                            <p><strong>Showered: </strong>{this.props.showered}</p>
+                            {this.props.exercise === "true" ?  
+                                <div>
+                                    <p><strong>Exercised: </strong>{this.props.exercise}</p>
+                                    <p><strong>Exercise Details: </strong>
+                                    <br></br>
+                                    {this.props.exerciseAmount}</p>
+                                </div>
+                            :   ""  }
+                            <p><strong>Daily Log: </strong>
+                            <br></br>
+                            {this.props.dailyLog}</p>
+                            <button type="button" className="btn btn-success float2" onClick={this.switchToEdit}>Edit this entry</button>
+                        </div>
                     </div>
-                </div>
-                <hr/>
-                <h5>How are you feeling today?</h5>
-                <div className="row">
-                    <div className="col-sm-12">
-                        <Slider 
-                            className="slider"
-                            name={"Mood"}
-                            display={this.state.Mood}
-                            update={this.updateValue}
-                            defaultValue={this.state.Mood}
-                        />
-                        <Slider 
-                            className="slider"
-                            name={"Anxiety"}
-                            display={this.state.Anxiety}
-                            update={this.updateValue}
-                            defaultValue={this.state.Anxiety}
-                        />
-                        <Slider
-                            className="slider" 
-                            name={"Energy"}
-                            display={this.state.Energy}
-                            update={this.updateValue}
-                            defaultValue={this.state.Energy}
-                        />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm-12">
-                        <BooleanInput
-                            name={"MedicineTaken"}
-                            title={"Medicine Taken"}
-                            update={this.updateValue}
-                        />
-                        <BooleanInput
-                            name={"Exercise"}
-                            title={"Exercise"}
-                            update={this.updateValue}
-                        />
-                          <BooleanInput
-                            name={"Showered"}
-                            title={"Showered"}
-                            update={this.updateValue}
-                        />
-                        <DropDownInput
-                            title={"Hours Slept: "}
-                            name="SleepHours"
-                            update={this.updateValue}
-                            defaultValue={this.state.SleepHours}
-                        />
-                    </div>
-                </div> 
-                <div className="row">
-                    <div className="col-sm-12">
-                    {this.state.Exercise === "true" ?
-                        <Input
-                            name={"ExerciseAmount"}
-                            title={"Exercise Details"}
-                            placeholder={"Log your exercise details here"}
-                            update={this.updateValue}
-                        /> : ""
-                    }
-                        <Input
-                            name="DailyLog"
-                            title={"Daily Log"}
-                            placeholder={"Log your thoughts about today here"}
-                            update={this.updateValue}
-                        />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm-9">
-                    {this.state.logged && 
-                         <button onClick={this.switchtoSeeData} className="btn btn-danger float-left">Back</button>
-                        }
-                    </div>
-                   
-                    <div className="col-sm-3">
-                        <button onClick={this.submitNewEntry} className="btn btn-success float">Submit</button>
+                : (
+                    <div className="jumbotron">
+                        <div className="row">
+                            <div className="col-sm-12 top">
+                                <h5>New Entry for: {entryDate}</h5>
+                            </div>
+                        </div>
+                        <hr/>
+                        <h5>How are you feeling today?</h5>
+                        <div className="row">
+                            <div className="col-sm-12">
+                                <Slider 
+                                    className="slider"
+                                    name={"Mood"}
+                                    display={this.state.Mood}
+                                    update={this.updateValue}
+                                    defaultValue={this.state.Mood}
+                                />
+                                <Slider 
+                                    className="slider"
+                                    name={"Anxiety"}
+                                    display={this.state.Anxiety}
+                                    update={this.updateValue}
+                                    defaultValue={this.state.Anxiety}
+                                />
+                                <Slider
+                                    className="slider" 
+                                    name={"Energy"}
+                                    display={this.state.Energy}
+                                    update={this.updateValue}
+                                    defaultValue={this.state.Energy}
+                                />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-12">
+                                <BooleanInput
+                                    name={"MedicineTaken"}
+                                    title={"Medicine Taken"}
+                                    update={this.updateValue}
+                                />
+                                <BooleanInput
+                                    name={"Exercise"}
+                                    title={"Exercise"}
+                                    update={this.updateValue}
+                                />
+                                <BooleanInput
+                                    name={"Showered"}
+                                    title={"Showered"}
+                                    update={this.updateValue}
+                                />
+                                <DropDownInput
+                                    title={"Hours Slept: "}
+                                    name="SleepHours"
+                                    update={this.updateValue}
+                                    defaultValue={this.state.SleepHours}
+                                />
+                            </div>
+                        </div> 
+                        <div className="row">
+                            <div className="col-sm-12">
+                            {this.state.Exercise === "true" ?
+                                <Input
+                                    name={"ExerciseAmount"}
+                                    title={"Exercise Details"}
+                                    placeholder={"Log your exercise details here"}
+                                    update={this.updateValue}
+                                /> : ""
+                            }
+                                <Input
+                                    name="DailyLog"
+                                    title={"Daily Log"}
+                                    placeholder={"Log your thoughts about today here"}
+                                    update={this.updateValue}
+                                />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-9">
+                            {this.state.logged && 
+                                <button onClick={this.switchtoSeeData} className="btn btn-danger float-left">Back</button>}
+                            </div>
                         
+                            <div className="col-sm-3">
+                                <button onClick={this.submitNewEntry} className="btn btn-success float">Submit</button>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-             )}
-           
-         </div>    
+                )}           
+            </div>    
         )
     }
     
